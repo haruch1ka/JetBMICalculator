f87a7fec177006ac45b7c4887b27077aa25f90fd
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-06-05 14:58:10 +0900

    MainActivity.ktにBMI計算用のUI要素を追加し、MainViewModel.ktを新規作成 27

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.example.jetbmicalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -38,9 +43,81 @@ class MainActivity : ComponentActivity() {
                             fontSize = 26.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
+                        Spacer(modifier = Modifier.padding(20.dp))
+
+                        //身長
+                        PinkLabeledTextField(
+                            value = "",
+                            onValueChange = { /*TODO*/ },
+                            label = "身長(cm)",
+                            placeholder = "170"
+                        )
+                        Spacer(modifier = Modifier.padding(10.dp))
+
+                        //体重
+                        PinkLabeledTextField(
+                            value = "",
+                            onValueChange = { /*TODO*/ },
+                            label = "体重(kg)",
+                            placeholder = "65"
+                        )
+                        Spacer(modifier = Modifier.padding(10.dp))
+
+                        //BMI計算ボタン
+                        Button(
+                            onClick = { /*TODO*/ },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "BMIを計算",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Spacer(modifier = Modifier.padding(20.dp))
+
+                        //BMIの結果
+                        Text(
+                            text = "あなたのBMIは22.5です",
+                            color = Color.Gray,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+
+                            )
+
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PinkLabeledTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    placeholder: String,
+) {
+    Column() {
+        Text(
+            text = label,
+            color = Color(0xFFF85F6A),
+            fontWeight = FontWeight.Bold,
+        )
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = value,
+            onValueChange = onValueChange,
+            placeholder = { Text(text = placeholder, color = Color.Gray) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+            )
+        )
+
     }
 }
---
+++ app/src/main/java/com/example/jetbmicalculator/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.jetbmicalculator
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    
+}
